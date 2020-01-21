b18373bf0f1619e09dff76a4e925472330a96125
TimmyC101 <tcalhoun101@yahoo.com>
2020-01-21 13:13:02 -0600

    Updates

---
+++ precache-manifest.6951cce615ea579678811be010fbf93c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42896794aabe37957423a4720f731892",
+    "url": "/single-page-app/index.html"
+  },
+  {
+    "revision": "794eff1e9eae0a0d6f2a",
+    "url": "/single-page-app/static/css/main.936a9a18.chunk.css"
+  },
+  {
+    "revision": "e57b14e3bbedc98742f7",
+    "url": "/single-page-app/static/js/2.0b47f818.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/single-page-app/static/js/2.0b47f818.chunk.js.LICENSE"
+  },
+  {
+    "revision": "794eff1e9eae0a0d6f2a",
+    "url": "/single-page-app/static/js/main.0c972f29.chunk.js"
+  },
+  {
+    "revision": "770401fa86d65b4a9eb4",
+    "url": "/single-page-app/static/js/runtime-main.cb4dcb8c.js"
+  },
+  {
+    "revision": "d9d2578068ea0ae9f7495ddc7c99a76e",
+    "url": "/single-page-app/static/media/paper.d9d25780.jpg"
+  }
+]);
